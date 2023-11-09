ea7a4d90afad5b5feb9d0f5f07c06d2971f7635e
PondPhuket <narongrid.dev@gmail.com>
2023-11-10 01:16:59 +0700

    research-detail => add loading page

--- src/views/public/research/DetailResearch.tsx
+++ src/views/public/research/DetailResearch.tsx
@@ -16,9 +16,12 @@ export default function DetailResearch() {
   const userInfo = useSelector((state: IRootState) => state.RDauth.user);
   const { id } = useLocation()?.state ?? "";
   const [raw, setRaw] = useState<IResearch | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   async function fetchData(userId: string, id: number) {
+    setIsLoading(true);
     const res: Omit<IResponse<IResearch>, 'pagin'> = await GetResearchDetailByUserId(userId, id);
+    setIsLoading(false);
     if (res && (res.statusCode === 200 && res.taskStatus)) {
       setRaw(res.data);
     }
@@ -54,83 +57,96 @@ export default function DetailResearch() {
   useEffect(() => {
     !id && navigate("/research");
     userInfo?.id && Promise.all([fetchData(userInfo.id, id)]);
-  }, [id, userInfo?.id]);
+  }, [id, userInfo?.id, navigate]);
 
   return (
     <Fragment>
-      {raw ? (
-        <div className="p-5 px-1 md:px-5 flex flex-grow flex-wrap w-full bg-white shadow-md dark:bg-zinc-800 rounded-xl">
-          <div className="flex justify-center w-full lg:w-6/12 border p-[1px] lg:h-[842px] lg:min-w-[595px]">
-            <img src={raw?.image_url as string} className="object-cover" alt="" />
+      {isLoading ?
+        <div className="p-5 px-1 md:px-5 flex flex-grow flex-wrap w-full bg-theme shadow-md rounded-xl">
+          <div className="animate-pulse bg-zinc-900 rounded flex justify-center w-full lg:w-6/12 p-[1px] lg:h-[842px] lg:min-w-[595px]"> </div>
+          <div className="h-full space-y-2 grow pl-2">
+            <div className="h-36 bg-zinc-900 rounded-md animate-pulse"></div>
+            <div className="h-14 bg-zinc-900 rounded-md animate-pulse"></div>
+            <div className="h-20 bg-zinc-900 rounded-md animate-pulse"></div>
+            <div className="h-10 bg-zinc-900 rounded-md animate-pulse"></div>
+            <div className="h-10 bg-zinc-900 rounded-md animate-pulse"></div>
+            <div className="h-14 bg-zinc-900 rounded-md animate-pulse"></div>
+            <div className="h-20 bg-zinc-900 rounded-md animate-pulse"></div>
           </div>
-          <fieldset className="grid place-self-start w-full flex-none lg:flex-1 pl-0 lg:pl-3">
-            <div className="relative w-full flex flex-col border-b border-zinc-700 p-5">
-              <h2 className="text-2xl font-semibold">{raw?.title}</h2>
-              <span>{raw?.title_alternative}</span>
-              <StarRating isChange onClickStar={handleOnClickRating} rating={raw?.average_rating} />
-              {/* like */}
-              <div onClick={() => handleLike(raw.id)} className="absolute top-0 right-0 text-center">
-                <div className="cursor-pointer p-2 w-10 h-10 hover:bg-zinc-700 rounded-full">
-                  {raw.like ?
-                    <FontAwesomeIcon className="text-2xl" icon={["fas", "heart"]} />
-                    :
-                    <FontAwesomeIcon className="text-2xl" icon={["far", "heart"]} />
-                  }
-                </div>
-                {FormatterNumber(raw.likes_count)}
-              </div>
-            </div>
-
-            <div className="detail-text-container">
-              <p className="w-3/12">จัดทำโดย</p>
-              <span className="detail-text">{`${raw?.user_info?.prefix}${raw?.user_info?.first_name} ${raw?.user_info?.last_name}`}</span>
-            </div>
-
-            <div className="detail-text-container">
-              <p className="w-3/12">รายวิชา</p>
-              <span className="detail-text">{raw?.subject}</span>
-            </div>
-
-            <div className="detail-text-container">
-              <p className="w-3/12">คำอธิบาย</p>
-              <span className="detail-text indent-5">{raw?.description}</span>
-            </div>
-
-            <div className="detail-text-container">
-              <p className="w-3/12">สำนักพิมพ์</p>
-              <span className="detail-text">{raw?.publisher}</span>
-            </div>
-
-            <div className="detail-text-container">
-              <p className="w-3/12">ผู้ให้ข้อมูล</p>
-              <span className="detail-text">{raw?.contributor}</span>
-            </div>
-
-            <div className="detail-text-container">
-              <p className="w-3/12">วันที่</p>
-              <span className="detail-text">{raw?.created_date}</span>
-            </div>
-
-            <div className="detail-text-container">
-              <p className="w-3/12">แหล่งที่มา</p>
-              <span className="detail-text">{raw?.source}</span>
-            </div>
-
-            <div className="detail-text-container">
-              <p className="w-3/12">สิทธิ์</p>
-              <span className="detail-text">{raw?.rights}</span>
-            </div>
-
-            <div className="detail-text-container">
-              <p className="w-3/12">แนบไฟล์</p>
-              <span className="w-full flex items-center justify-between lg:w-8/12 text-sm border-gray-300 dark:border-zinc-600 border rounded-lg p-2">
-                {(raw?.file_url as string)?.split("/").slice(-1)[0]}
-                <a className="text-xs text-indigo-600" href="http://">Download</a>
-              </span>
-            </div>
-          </fieldset>
         </div>
-      ) : null}
+        : raw ? (
+          <div className="p-5 px-1 md:px-5 flex flex-grow flex-wrap w-full bg-white shadow-md dark:bg-zinc-800 rounded-xl">
+            <div className="flex justify-center w-full lg:w-6/12 border p-[1px] lg:h-[842px] lg:min-w-[595px]">
+              <img src={raw?.image_url as string} className="object-cover" alt="" />
+            </div>
+            <fieldset className="grid place-self-start w-full flex-none lg:flex-1 pl-0 lg:pl-3">
+              <div className="relative w-full flex flex-col border-b border-zinc-700 p-5">
+                <h2 className="text-2xl font-semibold">{raw?.title}</h2>
+                <span>{raw?.title_alternative}</span>
+                <StarRating isChange onClickStar={handleOnClickRating} rating={raw?.average_rating} />
+                {/* like */}
+                <div onClick={() => handleLike(raw.id)} className="absolute top-0 right-0 text-center">
+                  <div className="cursor-pointer p-2 w-10 h-10 hover:bg-zinc-700 rounded-full">
+                    {raw.like ?
+                      <FontAwesomeIcon className="text-2xl" icon={["fas", "heart"]} />
+                      :
+                      <FontAwesomeIcon className="text-2xl" icon={["far", "heart"]} />
+                    }
+                  </div>
+                  {FormatterNumber(raw.likes_count)}
+                </div>
+              </div>
+
+              <div className="detail-text-container">
+                <p className="w-3/12">จัดทำโดย</p>
+                <span className="detail-text">{`${raw?.user_info?.prefix}${raw?.user_info?.first_name} ${raw?.user_info?.last_name}`}</span>
+              </div>
+
+              <div className="detail-text-container">
+                <p className="w-3/12">รายวิชา</p>
+                <span className="detail-text">{raw?.subject}</span>
+              </div>
+
+              <div className="detail-text-container">
+                <p className="w-3/12">คำอธิบาย</p>
+                <span className="detail-text indent-5">{raw?.description}</span>
+              </div>
+
+              <div className="detail-text-container">
+                <p className="w-3/12">สำนักพิมพ์</p>
+                <span className="detail-text">{raw?.publisher}</span>
+              </div>
+
+              <div className="detail-text-container">
+                <p className="w-3/12">ผู้ให้ข้อมูล</p>
+                <span className="detail-text">{raw?.contributor}</span>
+              </div>
+
+              <div className="detail-text-container">
+                <p className="w-3/12">วันที่</p>
+                <span className="detail-text">{raw?.created_date}</span>
+              </div>
+
+              <div className="detail-text-container">
+                <p className="w-3/12">แหล่งที่มา</p>
+                <span className="detail-text">{raw?.source}</span>
+              </div>
+
+              <div className="detail-text-container">
+                <p className="w-3/12">สิทธิ์</p>
+                <span className="detail-text">{raw?.rights}</span>
+              </div>
+
+              <div className="detail-text-container">
+                <p className="w-3/12">แนบไฟล์</p>
+                <span className="w-full flex items-center justify-between lg:w-8/12 text-sm border-gray-300 dark:border-zinc-600 border rounded-lg p-2">
+                  {(raw?.file_url as string)?.split("/").slice(-1)[0]}
+                  <a className="text-xs text-indigo-600" href="http://">Download</a>
+                </span>
+              </div>
+            </fieldset>
+          </div>
+        ) : null}
     </Fragment>
   )
 }
