d89638fbaad317e4d341a314525707f2f78f8b94
PondPhuket <narongrid.dev@gmail.com>
2023-11-14 20:32:02 +0700

    axios => แก้ไข get token

--- src/services/axios.service.ts
+++ src/services/axios.service.ts
@@ -1,12 +1,22 @@
 import axios from "axios";
-const BASE_URL = "https://research-server.vercel.app/api/";
-// const BASE_URL = "http://localhost:3001/api/";
+// const BASE_URL = "https://research-server.vercel.app/api/";
+const BASE_URL = "http://localhost:3001/api/";
 
 const axiosService = axios.create({
   baseURL: BASE_URL,
   headers: {
     "Access-Control-Allow-Origin": "*",
   },
+});
+
+axiosService.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token")?.replace(/^"(.*)"$/, '$1');
+  if (token) {
+    config.headers['Authorization'] = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export { BASE_URL };
